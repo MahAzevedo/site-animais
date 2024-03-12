f5b3ada56d625d0be49ace4a62b48d1bf77a4e48
MahAzevedo <madavetto@gmail.com>
2024-03-12 00:12:45 -0300

    modified: selecionar-o-dom/script.js

--- selecionar-o-dom/script.js
+++ selecionar-o-dom/script.js
@@ -473,3 +473,9 @@ if (h2rect.top < 0) {
 // machtMedia():
 // machtMedia() : melhor usar esse aqui para verificar altura do browser.
 
+const small = window.matchMedia('(max-width: 600px)');
+if (small.matches) {
+    console.log('Tela menor que 600px')
+} else {
+    console.log('Tela maior que 600px')
+};
