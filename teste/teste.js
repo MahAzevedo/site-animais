a843a18de75ed071e4ead6cb2cb8f196036c5165
MahAzevedo <madavetto@gmail.com>
2024-03-28 19:32:32 -0300

    modified:   teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -4,6 +4,7 @@ if (grupoA > grupoB) {
     console.log('Grupo A, ganhou!')
 } else {
     console.log('Grupo B, ganhou!')
-
 }
 
+const vencedor = grupoA > grupoB ? 'Grupo A, ganhou!' : 'Grupo B, ganhou!';
+console.log(vencedor)
