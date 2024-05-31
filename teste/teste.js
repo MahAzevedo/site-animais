3a3b106f441da5f962e3aea064a76996621f0d2b
MahAzevedo <madavetto@gmail.com>
2024-05-31 18:49:01 -0300

    modified:   teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -15,5 +15,5 @@ console.log(vencedor)
 // com ele dá ora fazer expressão/expressões
 // () se usa parenteses para executar algo
 
-const vend = grupoA > grupoB ? 'Grupo A, ganhou!' : 'Grupo B, ganhou!';
+const ven = grupoA > grupoB ? 'Grupo A, ganhou!' : 'Grupo B, ganhou!';
 console.log(ven)
