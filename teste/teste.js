b83655f32051fb75a9663648fadefde700904ca8
MahAzevedo <madavetto@gmail.com>
2024-06-08 22:02:39 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -20,3 +20,4 @@ console.log(vencedor)
 
 // Mostre os dados da aplicação, como apresentado no vídeo
 // Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
