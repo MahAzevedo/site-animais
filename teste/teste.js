ab17306b3bf70f15f609b6316730f5dc50d883f1
MahAzevedo <madavetto@gmail.com>
2024-06-09 22:03:20 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -21,3 +21,4 @@ console.log(vencedor)
 // Mostre os dados da aplicação, como apresentado no vídeo
 // Não utilize CSS externo, use o style para mudar as cores
 // Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
