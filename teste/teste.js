7fc94de0317b33ef063f6f6090a54d668b11a473
MahAzevedo <madavetto@gmail.com>
2024-06-10 22:04:55 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -22,3 +22,5 @@ console.log(vencedor)
 // Não utilize CSS externo, use o style para mudar as cores
 // Se a situação estiver ativa pinte de verde, inativa vermelho
 // Se o gasto for maior que 10000 mostre uma mensagem
+
+const luana = {};
