7a251d81ca53b1a8101fef24d8591d233301ca2e
MahAzevedo <madavetto@gmail.com>
2024-06-11 22:09:06 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -23,3 +23,6 @@ console.log(vencedor)
 // Se a situação estiver ativa pinte de verde, inativa vermelho
 // Se o gasto for maior que 10000 mostre uma mensagem
 
+const App = () => {
+    return <button>Comprar</button>;
+};
