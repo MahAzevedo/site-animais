720917776e9694eb3ea8c70a5c471fcc5e106fee
MahAzevedo <madavetto@gmail.com>
2024-06-22 23:34:31 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -5,17 +5,4 @@
 
 
 
-const App = () => {
-    const estiloH1 = {
-        color: 'blue',
-        fontSize: '20px',
-        fontFamily: 'Helvetica',
-    };
-
-    return (
-        <div>
-            <h1 style={estiloH1}>Empresa</h1>
-            <p style={{ color: 'green' }}>Sempre aberta</p>
-        </div>
-    )
-};
+const 
