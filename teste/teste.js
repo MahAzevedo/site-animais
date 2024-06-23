8b65f6ce5d4df7a4848565c48537ee8da7d35de4
MahAzevedo <madavetto@gmail.com>
2024-06-22 23:53:59 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -15,3 +15,15 @@ const luana = {
     ],
     ativa: true,
 };
+
+const mario = {
+    cliente: 'Mario',
+    idade: 31,
+    compras: [
+        { nome: 'Notebook', preco: 'R$ 2500' },
+        { nome: 'Geladeira', preco: 'R$ 3000' },
+        { nome: 'Smartphone', preco: 'R$ 1500' },
+        { nome: 'Guitarra', preco: 'R$ 3500' },
+    ],
+    ativa: false,
+};
