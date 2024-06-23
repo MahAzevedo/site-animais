c15b49420d667c59806b605fe243e7ed01b47b95
MahAzevedo <madavetto@gmail.com>
2024-06-22 23:32:10 -0300

    modified: teste/teste.js

--- teste/teste.js
+++ teste/teste.js
@@ -1,30 +1,21 @@
-const grupoA = 100;
-const grupoB = 200;
-if (grupoA > grupoB) {
-    console.log('Grupo A, ganhou!')
-} else {
-    console.log('Grupo B, ganhou!')
-}
-
-const vencedor = grupoA > grupoB ? 'Grupo A, ganhou!' : 'Grupo B, ganhou!';
-console.log(vencedor)
-
-// operador ternario
-// resume em menos linhas o código
-// usa simbologia
-// com ele dá ora fazer expressão/expressões
-// () se usa parenteses para executar algo
-
-
-//****************
-
 // Mostre os dados da aplicação, como apresentado no vídeo
 // Não utilize CSS externo, use o style para mudar as cores
 // Se a situação estiver ativa pinte de verde, inativa vermelho
 // Se o gasto for maior que 10000 mostre uma mensagem
 
-const Titulo = <h1>Meu Título</h1>;
+
 
 const App = () => {
-    return <div>{Titulo}</div>
+    const estiloH1 = {
+        color: 'blue',
+        fontSize: '20px',
+        fontFamily: 'Helvetica',
+    };
+
+    return (
+        <div>
+            <h1 style={estiloH1}>Empresa</h1>
+            <p style={{ color: 'green' }}>Sempre aberta</p>
+        </div>
+    )
 };
